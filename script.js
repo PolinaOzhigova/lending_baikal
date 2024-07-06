b90df89f59fc1c413fd416e119ae194027cee4b2
polina <117908658+PolinaOzhigova@users.noreply.github.com>
2024-07-06 11:14:20 +0800

    fix mobile adaptive menu

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function toggleMenu() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.classList.toggle('expanded');
-    const menu = document.getElementById('menu');
-    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-  }
-  
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.classList.toggle('expanded');
+  const menu = document.getElementById('menu');
+  menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+}
+
 function closeMenu() {
   const sidebar = document.querySelector('.sidebar');
   sidebar.classList.remove('expanded');
@@ -12,59 +12,64 @@ function closeMenu() {
   menu.style.display = 'none';
 }
 
+function showSubMenu(event) {
+  const item = event.currentTarget;
+  const sectionId = item.getAttribute('href').substring(1);
+  const section = document.getElementById(sectionId);
+
+  // Проверяем, если секции 2 или 6, то не добавляем подменю
+  if (sectionId === 'section2' || sectionId === 'section6') {
+    return;
+  }
+
+  if (section) {
+    const h2List = section.querySelectorAll('h2');
+    const submenu = document.createElement('ul');
+    h2List.forEach(h2 => {
+      const listItem = document.createElement('li');
+      const link = document.createElement('a');
+      link.textContent = h2.textContent;
+      link.setAttribute('href', `#${h2.id}`);
+      listItem.appendChild(link);
+      submenu.appendChild(listItem);
+
+      // Добавление обработчика для клика по пункту подменю
+      link.addEventListener('click', function() {
+        submenu.parentNode.removeChild(submenu);
+        closeMenu();
+      });
+    });
+
+    // Удаление предыдущего подменю, если есть
+    const existingSubmenu = item.parentNode.querySelector('.submenu');
+    if (existingSubmenu) {
+      existingSubmenu.parentNode.removeChild(existingSubmenu);
+    }
+
+    // Показать подменю
+    submenu.classList.add('submenu');
+    item.parentNode.appendChild(submenu);
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   const menuItems = document.querySelectorAll('.menu > ul > li > a');
-  // console.log(menuItems);
+  const isMobile = window.innerWidth <= 600;
+  // console.log(window.innerWidth)
+  
   menuItems.forEach(item => {
-    item.addEventListener('mouseenter', function() {
-      const sectionId = item.getAttribute('href').substring(1);
-      const section = document.getElementById(sectionId);
-      if (section) {
-        const h2List = section.querySelectorAll('h2');
-        const submenu = document.createElement('ul');
-        h2List.forEach(h2 => {
-          const listItem = document.createElement('li');
-          const link = document.createElement('a');
-          link.textContent = h2.textContent;
-          link.setAttribute('href', `#${h2.id}`);
-          listItem.appendChild(link);
-          submenu.appendChild(listItem);
-
-          // Добавление обработчика для клика по пункту подменю
-          link.addEventListener('click', function() {
-            submenu.parentNode.removeChild(submenu);
-            closeMenu();
-          });
-        });
-
-        // Удаление предыдущего подменю, если есть
-        const existingSubmenu = item.parentNode.querySelector('.submenu');
-        if (existingSubmenu) {
-          existingSubmenu.parentNode.removeChild(existingSubmenu);
-        }
-        // Показать подменю
-        submenu.classList.add('submenu');
-        item.parentNode.appendChild(submenu);
-        // Добавление события для подменю
-        submenu.addEventListener('mouseleave', function() {
-          submenu.parentNode.removeChild(submenu);
-        });
-      }
-    });
-    item.addEventListener('mouseleave', function() {
-      const submenu = item.parentNode.querySelector('.submenu');
-      if (submenu) {
-        submenu.addEventListener('mouseenter', function() {
-          submenu.style.display = 'block';
-        });
-        submenu.addEventListener('mouseleave', function() {
-          submenu.parentNode.removeChild(submenu);
-        });
-      }
-    });
+    if (isMobile) {
+      item.addEventListener('click',function() {
+        closeMenu();
+      });;
+    } else {
+      item.addEventListener('click',function() {
+        closeMenu();
+      });
+      item.addEventListener('mouseenter', showSubMenu);
+    }
   });
 });
-
 
 document.addEventListener("DOMContentLoaded", function() {
   const description = document.getElementById("description");
